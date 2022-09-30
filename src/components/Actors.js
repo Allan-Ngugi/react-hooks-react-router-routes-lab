9d030d4ae14d86cbf266d88870b8ea3feab50b25
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-09-30 08:44:24 +0300

    Complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,24 @@
 import React from "react";
+//import ReactDOM from "react-dom";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const getActors = actors.map((actor)=>(
+    <div key={actor.name} >
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map((movie)=>(
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+
+  )) 
+
+  return <div> <h1>Actors Page</h1>
+  {getActors}
+  </div>;
 }
 
 export default Actors;
+//ReactDOM.render(<Actors />, document.getElementById("root"));
